63bf48c9a68347cc7e65540af9967cf856561952
mito <raimiti8@gmail.com>
2021-05-02 11:41:02 +0900

    mainmemuにホバーがあたる

--- copy_RT.js
+++ copy_RT.js
@@ -59,7 +59,7 @@ $('.c-button.close').on('click',function(){     // クリックすると
 // });
 
 $(function () {
-  $('.p-gnavi__main-list').click( function () {
+  $('.p-gnavi__main-list').hover( function () {
 
     $(this).children('.p-gnavi__sub').slideToggle();
 
